7b3f1f72822f5899645c3f98b0b419141aafd223
CityMado <stenius.jere@gmail.com>
2022-04-14 19:35:30 +0300

    Started work on the calculator

---
+++ Assets/Scripts/Calculator/ButtonScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public enum NumberButton {Zero = 0,
+    One = 1, 
+    Two = 2, 
+    Three = 3, 
+    Four = 4, 
+    Five = 5, 
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Equals,
+    Plus,
+    Divide, 
+    Multiply,
+    Restart
+     };
+    public NumberButton numbers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
