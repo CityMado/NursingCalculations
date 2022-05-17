7991531875e5afc1128b8c90ca351371ba3a805c
CityMado <stenius.jere@gmail.com>
2022-05-17 21:17:26 +0300

    filters and needles can now be attached

--- Assets/Scripts/PerfursorScript.cs
+++ Assets/Scripts/PerfursorScript.cs
@@ -9,6 +9,7 @@ public class PerfursorScript : MonoBehaviour
     public GameObject attachedItem;
     public bool perfursorAttached = false;
     public bool syringeAttached = false;
+    public bool needleFilterAttached = false;
 
     private void Awake()
     {
@@ -25,12 +26,19 @@ public class PerfursorScript : MonoBehaviour
 
             //other.gameObject.transform.position = attachPoint.transform.position;
         }
-        if(other.gameObject.tag == "Syringe")
+        if(other.gameObject.tag == "Needle")
         {
             syringeAttached = true;
             Destroy(other.gameObject);
             var newSyringe = Instantiate(attachedItem, attachPoint.transform.position, attachPoint.transform.rotation);
             newSyringe.transform.parent = attachPoint.transform;
-        }   
+        }  
+        if(other.gameObject.tag == "NeedleFilter")
+        {
+            needleFilterAttached = true;
+            Destroy(other.gameObject);
+            var newFilter = Instantiate(attachedItem, attachPoint.transform.position, attachPoint.transform.rotation);
+            newFilter.transform.parent = attachPoint.transform;
+        } 
     }
 }
